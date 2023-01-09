c2e5cdcb95f3b0c7a578ef03fca7a1de1cf6fbaa
Count Cipher <chrisharlow366@yahoo.com>
2023-01-08 19:34:05 -0500

    com

--- resources/views/components/listing-card.blade.php
+++ resources/views/components/listing-card.blade.php
@@ -12,28 +12,10 @@
                 <a href="listings/{{$listing->id}}">{{$listing->title}}</a>
             </h3>
             <div class="text-xl font-bold mb-4">{{$listing->company}}</div>
-            <ul class="flex">
-                <li
-                    class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs"
-                >
-                    <a href="#">Laravel</a>
-                </li>
-                <li
-                    class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs"
-                >
-                    <a href="#">API</a>
-                </li>
-                <li
-                    class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs"
-                >
-                    <a href="#">Backend</a>
-                </li>
-                <li
-                    class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs"
-                >
-                    <a href="#">Vue</a>
-                </li>
-            </ul>
+
+            {{-- This is passing in a prop.  In this case, it's going to the 'listing-tags' component --}}
+            <x-listing-tags :tagsCsv="$listing->tags"/>
+
             <div class="text-lg mt-4">
                 <i class="fa-solid fa-location-dot"></i> {{$listing->location}}
             </div>
