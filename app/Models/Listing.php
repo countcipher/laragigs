b3f0ccdd98c3ab33629e65d80467785cadea4d3b
Count Cipher <chrisharlow366@yahoo.com>
2023-01-14 10:10:46 -0500

    added search by company functionality

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -22,7 +22,8 @@ class Listing extends Model
         if($filters['search'] ?? false){
             $query->where('title', 'like', '%' . request('search') . '%')
             ->orWhere('description', 'like', '%' . request('search') . '%')
-            ->orWhere('tags', 'like', '%' . request('search') . '%');
+            ->orWhere('tags', 'like', '%' . request('search') . '%')
+            ->orWhere('company', 'like', '%' . request('search') . '%');
         }
     }
 
